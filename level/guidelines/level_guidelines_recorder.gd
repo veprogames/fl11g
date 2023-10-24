757255e37b74db0da6b0803d85cd3effdb58db5c
veprogames <75524847+veprogames@users.noreply.github.com>
2023-10-24 10:10:36 +0200

    build first level

--- level/guidelines/level_guidelines_recorder.gd
+++ level/guidelines/level_guidelines_recorder.gd
@@ -10,12 +10,21 @@ var data := LevelGuidelineData.new()
 func is_recording() -> bool:
 	return recording and OS.is_debug_build()
 
+func is_empty() -> bool:
+	return len(data.lines) == 0
+
+func save() -> void:
+	var filename := "res://recorded_guidelines/guidelines-%s-%s.tres" % [file_name_extra, Time.get_time_string_from_system()]
+	ResourceSaver.save(data, filename)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if is_recording() and Input.is_action_just_pressed("guidelines_add_line"):
-		data.add_line(player_manager.get_highest_player_x())
+func _process(_delta: float) -> void:
+	if is_recording():
+		if Input.is_action_just_pressed("guidelines_add_line"):
+			data.add_line(player_manager.get_highest_player_x())
+		if Input.is_action_just_pressed("guidelines_save"):
+			save()
 
 func _exit_tree() -> void:
-	if is_recording():
-		ResourceSaver.save(data, "res://guidelines-%s-%s.tres" % [file_name_extra, Time.get_time_string_from_system()])
-		#ResourceSaver.save(data, "user://guidelines-%s-%s.tres" % [file_name_extra, Time.get_time_string_from_system()])
+	if is_recording() and not is_empty():
+		save()
--- level/player_camera_manager.gd
+++ level/player_camera_manager.gd
@@ -5,11 +5,15 @@ extends Node2D
 @export var player_container: Node2D
 @export var target_spawner: PlayerSpawner
 
+@export var follow_player_y := true
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	var target_player := get_farthest_player()
 	if target_player:
-		camera.global_position = target_player.position
+		camera.global_position.x = target_player.position.x
+		if follow_player_y:
+			camera.global_position.y = target_player.position.y
 
 func wait_and_go_to_spawner():
 	await get_tree().create_timer(0.75).timeout
