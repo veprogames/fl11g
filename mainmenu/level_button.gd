ea802aacc58dcfcee768645b2fbd9d5227c4167a
veprogames <75524847+veprogames@users.noreply.github.com>
2023-11-05 09:59:03 +0100

    Don't show test level in release builds

--- mainmenu/level_button.gd
+++ mainmenu/level_button.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var level_data: LevelData
+@export var debug_only := false
 
 @onready var label_title: Label = $Content/Info/LabelTitle
 @onready var label_percent: Label = $Content/Info/LabelPercent
@@ -8,6 +9,9 @@ extends Control
 var Scene: PackedScene
 
 func _ready() -> void:
+	if debug_only and not OS.is_debug_build():
+		queue_free()
+	
 	Scene = load(level_data.scene_path)
 	label_title.text = level_data.title
 	label_percent.text = "%02d%%" % (level_data.get_current_percentage() * 100)
